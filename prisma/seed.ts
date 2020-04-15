0bb911cd3ef3f0b0768b771b86404552bd149bd8
sashman <sashman90@gmail.com>
2020-04-15 22:28:12 +0100

    Fixing participant relation

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,15 +5,17 @@ const db = new PrismaClient()
 main()
 
 async function main() {
-  console.log('Deleting users')
+  console.log('Deleting participants')
+  await db.participant.deleteMany({})
 
+  console.log('Deleting users')
   await db.user.deleteMany({})
 
   console.log('Deleting matches')
   await db.match.deleteMany({})
 
-  console.log('Deleting participants')
-  await db.participant.deleteMany({})
+  console.log('Creating match')
+  const match = await db.match.create({ data: {} })
 
   console.log('Creating users')
   const userData = new Array(5).fill(0).map((_, i) => ({
@@ -25,19 +27,53 @@ async function main() {
     userData.map((data) => db.user.create({ data })),
   )
 
-  console.log('Creating match')
-  const match = await db.match.create({
+  const participant = await db.participant.create({
     data: {
-      participants: {
-        create: users.map((user) => ({
-          participant: { connect: { id: user.id } },
-        })),
+      match: {
+        connect: {
+          id: match.id,
+        },
+      },
+      user: {
+        connect: {
+          id: users[0].id,
+        },
       },
     },
   })
+
+  const participants = await Promise.all(
+    users.map((user) =>
+      db.participant.create({
+        data: {
+          match: {
+            connect: {
+              id: match.id,
+            },
+          },
+          user: {
+            connect: {
+              id: user.id,
+            },
+          },
+        },
+      }),
+    ),
+  )
+
+  // console.log('Creating match')
+  // const match = await db.match.create({
+  //   data: {
+  //     participants: {
+  //       create: users.map((user) => ({
+  //         participant: { connect: { id: user.id } },
+  //       })),
+  //     },
+  //   },
+  // })
   // connect: results.map(result => ({ id: result.id }))
 
-  console.log('Seeded: %j', { users, match })
+  console.log('Seeded: %j', { users, match, participant })
 
   db.disconnect()
 }
